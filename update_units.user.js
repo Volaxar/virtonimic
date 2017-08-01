84dd487d6bdd4ad83a068ba79a207f6a3c6df9c9
Volaxar <volaxar@gmail.com>
2017-08-01 15:57:28 +0500

    Объединение скриптов. Рефакторинг.

--- update_units.user.js
+++ update_units.user.js
@@ -8,71 +8,121 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     var hrefMatch = window.location.href.match(/https:\/\/virtonomica.ru\/(\w+)\/main\/unit\/view\/(\d+)\/supply/);
-    var realmName;
-    var unitId;
+    var realmName;  // Название реалма
+    var unitId; // ИД юнита
+    var unitTypeName;   // Название типа юнита
+    var product = {'id': 0, 'name': '', 'sale': {}, 'raws': {}};   // Производимый продукт
 
-    var getProductSale = function(pageSale, productId) {
-        var productLine = pageSale.children('form[name=storageForm]').find('a[href*=' + productId + ']').eq(0).parents('tr').eq(0);
-        var productSale = productLine.children('td').filter('[align=right]').eq(2).text().replace(' ', '');
-
-        return(productSale);
-    };
-
-    var setMaterialValue = function(raws, count) {
-        $('#mDestroy').find('tr[id^=r]').each(function() {
+    var setMaterialValue = function () {
+        $('#mDestroy').find('tr[id^=r]').each(function () {
             var line = $(this);
 
-            if(line.attr('id').length > 1) {
+            if (line.attr('id').length > 1) {
                 var materialName = line.children('td').eq(0).attr('title');
-                var multiplier = parseFloat(raws[materialName].count);
+                var multiplier = parseFloat(product['raws'][materialName]);
 
                 var cell = line.children('td').eq(6);
-                cell.append('</br><span class="pseudolink min-order">' + Math.ceil(parseInt(count) * multiplier) + '</span></br>');
+                var count = Math.ceil(parseInt(product['sale'][product['id']]) * multiplier);
+                cell.append('</br><span class="pseudolink min-order">' + count + '</span></br>');
             }
         });
-        $('span.min-order').css('margin-right', '3px');
+
+        $('span.min-order').css('margin-right', '3px').click(function () {
+            var spanCount = $(this);
+            var itemId = spanCount.parents('tr').eq(0).attr('id').replace('r', 'qc');
+            var input = $('#' + itemId);
+
+            input.val(spanCount.text());
+        });
     };
 
-    var loadCount = function(mainContent, materialName) {
-        localforage.getItem(materialName).then(function(value) {
-            if(value) {
-                var productSale = getProductSale(mainContent, value.id);
+    var loadRawData = function () {
+        localforage.getItem(materialName).then(function (value) {
+            product['id'] = value['id'];
+            product['raws'] = value['raws'];
 
-                setMaterialValue(value.raws, productSale);
+            setMaterialValue();
+        });
+    };
 
-                $('span.min-order').click(function() {
-                    var spanCount = $(this);
-                    var itemId = spanCount.parents('tr').eq(0).attr('id').replace('r', 'qc');
-                    var input = $('#' + itemId);
+    var saveRawData = function (unitTypeId) {
+        $.get('/' + realmName + '/main/industry/unit_type/info/' + unitTypeId, function (data) {
+            var productList = $(data).find('#mainContent table.grid:first > tbody > tr:gt(0)');
 
-                    input.val(spanCount.text());
+            productList.each(function () {
+                var productName = $(this).children('td').eq(0).text();
+
+                var raws = {};
+                $(this).children('td').eq(2).find('a:even').each(function () {
+                    var rawName = $(this).attr('title');
+
+                    var countText = $(this).parents('table').first().find('td:last').text().match(/[0-9\/]+/)[0];
+                    var count = parseInt(countText);
+                    var countRight = countText.match(/\/(\d+)/);
+                    if (countRight) {
+                        count = count / parseInt(countRight[1]);
+                    }
+
+                    raws[rawName] = count;
                 });
-            }
-        });
+
+                var productId = $(this).children('td').eq(3).find('a:first').attr('href').match(/^.*?(\d+)$/)[1];
+
+                if (productName == product['name']) {
+                    product['id'] = productId;
+                    product['raws'] = raws;
+                }
+
+                localforage.setItem('Материалы_' + productName, {
+                    'id': productId,
+                    'raws': raws
+                });
+            });
+
+            setMaterialValue();
+
+        }, 'html');
     };
 
-    var loadSale = function(data) {
-        var mainContent = $(data).find('#mainContent');
-        var specName = mainContent.children('table').eq(0).find('td').eq(2).text();
-        var materialName = 'Материалы_' + specName;
+    // Загружаем страницу сбыта.
+    // Получаем название специализации, типа юнита.
+    var loadSalePage = function (data) {
+        product['name'] = $(data).find('#mainContent table:first td:eq(2)').text();
 
-        localforage.keys().then(function(value) {
-            if(value.indexOf(materialName) != -1) {
-                loadCount(mainContent, materialName);
+        unitTypeName = $(data).find('#headerInfo h1').text();
+
+        var saleList = $(data).find('form[name=storageForm] table.grid > tbody > tr:gt(0)');
+
+        saleList.each(function () {
+            var orderLink = $(this).children('td').eq(-7).find('a');
+            var orderId = orderLink.attr('href').match(/^.*?(\d+)$/)[1];
+
+            product['sale'][orderId] = $(this).children('td').eq(-4).text().replace(' ', '');
+        });
+
+        // Проверяем наличие информации по составу продукта
+        localforage.keys().then(function (value) {
+            if (value.indexOf(product['name']) != -1) {
+                loadRawData();
             } else {
-                alert('Информация о количестве материалов не загруженна');
+                $.get('/' + realmName + '/main/common/main_page/game_info/industry', function (data) {
+                    var unitTypeHref = $(data).find('a:contains(' + unitTypeName + ')').attr('href');
+                    var unitTypeId = unitTypeHref.match(/\/(\d+)$/)[1];
+
+                    saveRawData(unitTypeId);
+                }, 'html');
             }
         });
     };
 
-    if(hrefMatch) {
+    if (hrefMatch) {
         realmName = hrefMatch[1];
         unitId = hrefMatch[2];
-    }
 
-    $.get('/' + realmName + '/main/unit/view/' + unitId + '/sale/', loadSale, 'html');
+        $.get('/' + realmName + '/main/unit/view/' + unitId + '/sale/', loadSalePage, 'html');
+    }
 })();
